063aaafaec642f842df55499fc53556a73aba2c6
EddieSkarke <8904611+x86Since8088@users.noreply.github.com>
2023-03-12 01:29:34 -0600

    updates

---
+++ .pending/Data/Get-MGMTConfig.psm1
@@ -0,0 +1,111 @@
+function Get-MGMTConfig {
+    [cmdletbinding()]
+    param (
+        $Name
+    )
+    begin{
+        $PSSR             = $PSScriptRoot
+        $Invocation       = $MyInvocation
+        $DataFolder       = $PSSR  -replace '^(\\\\.*?|.*?)\\(.*\\.*)','$1\Data\$2'
+                            if (!(Test-Path $DataFolder)) {mkdir $DataFolder|out-null}
+        $ConfigFile       = "$DataFolder\Config.yaml"
+                            if (!(Test-Path $ConfigFile)) {
+                                [ordered]@{
+                                    Generated     = (get-date).ToString()
+                                    GeneratedBy   = $MyInvocation.ScriptName
+                                }
+                            }
+        try{
+            $Data = [system.io.file]::ReadAllLines($ConfigFile)|ConvertFrom-Yaml
+            if ($Name -match '\n') {
+                Get-DataObject -InputObject $Data -Name $Name
+            }
+            else {
+                $Data
+            }
+        }catch{
+            Write-Error -Message "Run Set-MGMTConfig and edit '$ConfigFile'."
+        }
+    }
+}
+
+function Set-MGMTConfig {
+    [cmdletbinding()]
+    param(
+        $Name,
+        $Value,
+        [switch]$PassThru
+    )
+    begin{
+        $Data = . Get-MGMTConfig 2> $Null
+        if ($null -eq $Data) {
+            $Data = [ordered]@{}
+            $Changed = $true
+        }
+        else {
+            $Changed = $False
+        }
+        if ($null -eq $Data['proxmox']  )   {$Data['proxmox']=@();$Changed=$true}
+        if ($Data['proxmox'].count -eq 0)   {
+                                                $Data['proxmox']=@(
+                                                    @{Server='';Node='';tags=@()}
+                                                )
+                                                $Changed=$true
+                                            }
+        if ($null -eq $Data['vmware']   )   {$Data['vmware']=@();$Changed=$true}
+        if ($Data['vmware'].count -eq 0)    {
+                                                $Data['vmware']=@(
+                                                    @{Server='';tags=@()}
+                                                )
+                                                $Changed=$true
+                                            }
+        if ($null -eq $Data['unraid']   )   {$Data['unraid']=@();$Changed=$true}
+        if ($Data['unraid'].count -eq 0)    {
+                                                $Data['unraid']=@(
+                                                    @{Server='';tags=@()}
+                                                )
+                                                $Changed=$true
+                                            }
+        if ($Changed) {
+            $Yaml = $Data|ConvertTo-Yaml
+            [system.io.file]::WriteAllLines($ConfigFile,$Yaml,[System.Text.Encoding]::UTF8)
+        }
+        if ($Name -match '\w') {
+            if ($null -ne $Value) {
+                Set-DataObject -InputObject $Data -Name $Name -Value $Name
+            }
+        }
+        if ($PassThru) {$Data}
+    }
+}
+
+function Set-DataObject {
+    param(
+        $InputObject,
+        $Name,
+        $Value
+    )
+    begin{
+        $NameSplit = $Name -split '\.'
+        $Reference = $InputObject
+        foreach ($NameItem in $NameSplit) {
+            $Reference=$Reference[$NameItem]
+        }
+        $Reference = $Value
+    }
+}
+
+function Get-DataObject {
+    param(
+        $InputObject,
+        $Name
+    )
+    begin{
+        $NameSplit = $Name -split '\.'
+        $Reference = $InputObject
+        foreach ($NameItem in $NameSplit) {
+            $Reference=$Reference[$NameItem]
+        }
+        return $Reference
+    }
+}
---
+++ PS_Module_Mgmt/Get-PSModuleLEvaluationTime.ps1
@@ -0,0 +1,21 @@
+﻿$Global:ModuleLoadTime = @{}
+$modules = Get-Module -ListAvailable | Select Name,Path,Version,ExportedFormatFiles,Guid,RootModule,PowerShellVersion
+foreach ($module in $modules) {
+    Write-Host "Evaluating module $($module.Name)..."
+    $startTime = Get-Date
+    $commands = Get-Command -Module $module.Name
+    $endTime = Get-Date
+    $timeTaken = $endTime - $startTime
+    Write-Host "Time taken to evaluate module $($module.Name): $($timeTaken.TotalMilliseconds)ms"
+    $FileLastWriteTimes = (gci (split-path $module.Path) -Recurse).LastWriteTime | sort | select -First 1 -Last 1
+    $Global:ModuleLoadTime[$module.Name]=[pscustomobject]@{
+        EvaluationTime = $timeTaken.TotalMilliseconds
+        ModuleName=$module.Name
+        Commands = $commands
+        Path = $module.Path
+        Version = $module.Version
+        LeastRecentWrite=$FileLastWriteTimes[0]
+        MostRecentWrite=$FileLastWriteTimes[-1]
+        Guid=$module.Guid
+    }
+}
