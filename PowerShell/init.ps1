fa50a8924f1a3bb19d61ef6dc1c2a2bc802a67cd
x86since8088 <8904611+x86Since8088@users.noreply.github.com>
2024-02-21 20:25:14 -0600

    named MGMTConfig.psm1

--- PowerShell/init.ps1
+++ PowerShell/init.ps1
@@ -1,7 +1,7 @@
 $WorkingFolder   = $PSScriptRoot
 $Datafolder      = $WorkingFolder -replace "^(.*?)\\(.*)$",'$1\Data\$2'
 $MGMTFolder      = $WorkingFolder -replace "^(.*?\\MGMT).*",'$1'
-Remove-Module -Name Get-MGMTConfig -ErrorAction Ignore -Force
+Remove-Module -Name MGMTConfig -ErrorAction Ignore -Force
 Import-Module "$MGMTFolder\PowerShell\Modules\Data\MGMTConfig" -DisableNameChecking -Verbose -Debug -Force
 if (!(Test-Path $Datafolder)) {New-Item -Path $Datafolder -ItemType Directory | Out-Null}
 
