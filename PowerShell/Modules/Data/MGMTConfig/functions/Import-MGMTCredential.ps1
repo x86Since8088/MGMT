1d81e1d6f4c5b24eaa00a39999946121107bdd73
x86since8088 <8904611+x86Since8088@users.noreply.github.com>
2024-04-25 23:30:36 -0500

    fix

--- PowerShell/Modules/Data/MGMTConfig/functions/Import-MGMTCredential.ps1
+++ PowerShell/Modules/Data/MGMTConfig/functions/Import-MGMTCredential.ps1
@@ -7,7 +7,7 @@ function Import-MGMTCredential {
     $AuthSave = Import-MGMTYAML -LiteralPath $global:MGMT_Env.AuthFile
     $YMLFileDate = (Get-Item $global:MGMT_Env.AuthFile).LastWriteTime
     if ($Global:MGMT_Env.Auth.LastWriteTime -eq $YMLFileDate) {return}
-    [byte[]]$Ukey = Merge-MGMTByteArray -ByteArray1 $global:MGMT_Env.Key -ByteArray2 $global:MGMT_Env.UShard
+    [byte[]]$Ukey = Merge-MGMTByteArray -ByteArray1 (ConvertFrom-MGMTBase64 -Base64 $global:MGMT_Env.Key) -ByteArray2 $global:MGMT_Env.UShard
     foreach ($SystemType in $AuthSave.Keys) {
         foreach ($SystemName in $AuthSave.($SystemType).Keys) {
             foreach ($CredItem in $AuthSave.($SystemType).($SystemName)) {
