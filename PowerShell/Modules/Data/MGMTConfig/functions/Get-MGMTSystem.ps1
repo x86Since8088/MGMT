d8e9e796d9b2df483e38408f8f4e168423a2aa58
x86since8088 <8904611+x86Since8088@users.noreply.github.com>
2024-04-28 20:03:56 -0500

    finalized Connect-MGMTViServer and Get-MGMTSystem.

--- PowerShell/Modules/Data/MGMTConfig/functions/Get-MGMTSystem.ps1
+++ PowerShell/Modules/Data/MGMTConfig/functions/Get-MGMTSystem.ps1
@@ -24,8 +24,13 @@ function Get-MGMTSystem {
                 [System.Management.Automation.Language.CommandAst] $CommandAst,
                 [System.Collections.IDictionary] $FakeBoundParameters
             )
-            return ($global:MGMT_Env.config.sites.($FakeBoundParameters['Environment']).SystemTypes.Keys |
-                Where-Object { $_ -like "*$WordToComplete*" }
+            return (
+                $global:MGMT_Env.config.sites.GetEnumerator()|
+                    Where-Object{$_.Key -like $FakeBoundParameters['Environment']}|
+                    Select-Object -ExpandProperty Value|
+                    Select-Object -ExpandProperty SystemTypes|
+                    Select-Object -ExpandProperty Keys|
+                    Where-Object { $_ -like "*$WordToComplete*" }
             )
         })]
         [string[]]$SystemType = '*',
@@ -38,35 +43,40 @@ function Get-MGMTSystem {
                 [System.Management.Automation.Language.CommandAst] $CommandAst,
                 [System.Collections.IDictionary] $FakeBoundParameters
             )
-            return ($global:MGMT_Env.config.sites.($FakeBoundParameters['Environment']).SystemTypes.($FakeBoundParameters['SystemType']).keys |
-                Where-Object { $_ -like "*$WordToComplete*" }
+            return (
+                $global:MGMT_Env.config.sites.GetEnumerator()|
+                    Where-Object{$_.Name -like $FakeBoundParameters['Environment']}|
+                    Select-Object -ExpandProperty Value|
+                    Select-Object -ExpandProperty SystemTypes|
+                    Where-Object{$_.keys -like $FakeBoundParameters['SystemType']}|
+                    ForEach-Object{$_.GetEnumerator()}|
+                    Select-Object -ExpandProperty Name|
+                    Where-Object{$_ -like "*$WordToComplete*"}
             )
         })]
         [string[]]$SystemName = '*'
     )
     begin {
         #Go through some gymnastics to get the hashtable keys and allow wildcards.
-        [string[]]$Environment  = $Environment | Where-Object{$_ -ne ''}
-        [string[]]$SystemType   = $SystemType | Where-Object{$_ -ne ''}
-        [string[]]$SystemName   = $SystemName | Where-Object{$_ -ne ''}
-        [string[]]$Enviornments = 
-        foreach ($EnvironmentItem in $Environment) {
-            ($global:MGMT_Env.config.sites).Keys |
-                Where-Object{
-                    $_ -like $EnvironmentItem
-                }
-        }
-        foreach ($Env in $Enviornments) {
-            [string[]]$SystemTypes = 
-                foreach($Type in $SystemType) {
-                    ($global:MGMT_Env.config.sites.($EnvironmentItem)).SystemTypes.Keys|
-                        Where-Object{$_ -like $Type}
-                }
+        [string[]]$Environment    = $Environment | Where-Object{$_ -ne ''}
+        [string[]]$SystemType     = $SystemType | Where-Object{$_ -ne ''}
+        [string[]]$SystemName     = $SystemName | Where-Object{$_ -ne ''}
+        [string[]]$Enviornments   = foreach ($EnvironmentItem in $Environment) {
+                                        ($global:MGMT_Env.config.sites).Keys |
+                                            Where-Object{
+                                                $_ -like $EnvironmentItem
+                                            }
+                                    }
+        foreach ($EnvironmentItem in $Enviornments) {
+            [string[]]$SystemTypes    = foreach($Type in $SystemType) {
+                                            ($global:MGMT_Env.config.sites.($EnvironmentItem)).SystemTypes.Keys|
+                                                Where-Object{$_ -like $Type}
+                                        }
             foreach ($Type in $SystemTypes) {
-                [array]$SystemNameKeys = foreach ($Name in $SystemName) {
-                    ($global:MGMT_Env.config.sites).($EnvironmentItem).SystemTypes.($Type).keys |
-                        Where-Object{$_ -like $Name}
-                }
+                [array]$SystemNameKeys    = foreach ($Name in $SystemName) {
+                                                ($global:MGMT_Env.config.sites).($EnvironmentItem).SystemTypes.($Type).keys |
+                                                    Where-Object{$_ -like $Name}
+                                            }
                 foreach ($SystemNameKey in $SystemNameKeys) {
                     [pscustomobject]@{
                         Environment = $EnvironmentItem
--- PowerShell/Modules/Virtualization/MGMTVMware/functions/Connect-MGMTViServer.ps1
+++ PowerShell/Modules/Virtualization/MGMTVMware/functions/Connect-MGMTViServer.ps1
@@ -1,25 +1,68 @@
 function Connect-MGMTViserver {
     [cmdletbinding()]
     param (
-        [string]$Server,
-        [pscredential]$Credential
+        [string[]]$Server,
+        [pscredential]$Credential,
+        [string]$UserName
     )
     begin {
         if ($null -eq $Server) {
             Write-Error -Message "The -Server parameter is required."
             return
         }
-        if ($null -eq $Credential) {
-            $Credential = @(
-                Get-MGMTCredential -SystemType VMware_Template -SystemName $Server
-                Get-MGMTCredential -SystemType VMware_vCenter  -SystemName $Server
-            ) | Select-Object -First 1
+        $MGMTCredParam = @{}
+        if ('' -ne $UserName) {
+            $MGMTCredParam.UserName = $UserName
         }
-        if ($null -ne $Credential) {
-            $ConnectVMHostPatams['Credential'] = $Credential
+        if ($null -eq $Credential) {
+            # If credentials are not set, we need to loop through the servers and set the credentials and stay in this behavior loop.
+            foreach ($ServerItem in $server) {
+                if ($ServerItem -match '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|^[:\da-f]{3,45}$') {
+                    # 0000:0000:0000:0000:0000:ffff:192.168.100.228 is a theorhedical fully expanded IPv6 address that maps to an IPv4 address and has 45 characters.
+                    $ResolvedIP = $ServerItem
+                }
+                else {
+                    $ResolvedIP = [System.Net.Dns]::GetHostEntry($ServerItem).IPAddress
+                }
+                if ($ServerItem -in $global:DefaultVIServers.Name) {
+                    Write-Host -Message "The server $ServerItem is already connected."
+                }
+                elseif ($ResolvedIP -in $global:DefaultVIServers.Name) {
+                    Write-Host -Message "The server $ServerItem (Resolved IP: $ResolvedIP) is already connected."
+                }
+                else {
+                    $SelecteVMwareServer = Get-MGMTSystem -Environment * -SystemType VMware* -SystemName *|
+                    Where-Object{
+                        ($_.data.ip -like $ServerItem) -or
+                        ($_.data.name -like $ServerItem) -or
+                        ($_.data.fqdn -like $ServerItem) 
+                    } | Select-Object -First 1
+                    $Credential = (Get-MGMTCredential -SystemType $SelecteVMwareServer.SystemType -SystemName $SelecteVMwareServer.SystemName @MGMTCredParam).Credential
+                    if ($null -eq $Credential) {
+                        Write-Error -Message "No credentials found for $ServerItem.`tUse the following command to set the credentials:`n`tSet-MGMTCredential -SystemType VMware_ESXi -SystemName $Server -Credential (Get-Credential)"
+                        Write-Warning -Message "Run:`n`tSet-MGMTCredential -SystemType $($SelecteVMwareServer.SystemType) -SystemName $($SelecteVMwareServer.SystemName) -Credential (Get-Credential)"
+                    }
+                    elseif ($null -eq $credential.password) {
+                        Write-Error -Message "The password for $ServerItem is not set.`tUse the following command to set the credentials:`n`tSet-MGMTCredential -SystemType VMware_ESXi -SystemName $Server -Credential (Get-Credential)"
+                        Write-Warning -Message "Run:`n`tSet-MGMTCredential -SystemType $($SelecteVMwareServer.SystemType) -SystemName $($SelecteVMwareServer.SystemName) -Credential (Get-Credential)"
+                    }
+                    else {
+                        Connect-VIServer -Server $ServerItem -Credential $Credential 1> $null 3> $null 4> $null 5> $null 6> $null 
+                    }
+                }
+            }
+        }
+        else {
+            # if the credential is set, we need to stay in this behavior loop.
             foreach ($ServerItem in $server) {
                 if ($ServerItem -notin $global:DefaultVIServers.Name) {
-                    Connect-VIServer @ConnectVMHostPatams 1> $null 3> $null 4> $null 5> $null 6> $null 
+                    if ($null -eq $credential.password) {
+                        Write-Error -Message "The password for $ServerItem is not set.`tUse the following command to set the credentials:`n`tSet-MGMTCredential -SystemType VMware_ESXi -SystemName $Server -Credential (Get-Credential)"
+                        Write-Warning -Message "Run:`n`tSet-MGMTCredential -SystemType [VMware_...] -SystemName [VMware...] -Credential (Get-Credential)"
+                    }
+                    else {
+                        Connect-VIServer -Server $ServerItem -Credential $Credential 1> $null 3> $null 4> $null 5> $null 6> $null 
+                    }
                 }
             }
         }
