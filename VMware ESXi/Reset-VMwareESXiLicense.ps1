970502badfb9b6bf967358384da7cd0166c8cb9f
x86since8088 <8904611+x86Since8088@users.noreply.github.com>
2025-04-23 00:00:44 -0500

    updated

--- VMware ESXi/Reset-VMwareESXiLicense.ps1
+++ VMware ESXi/Reset-VMwareESXiLicense.ps1
@@ -1,20 +1,15 @@
 ﻿param (
     [string]$ComputerName,
-    [pscredential]$Credential
+    [pscredential]$Credential = (.{
+        Get-MGMTCredential -SystemType VMware_ESXi -SystemName $ComputerName
+        Get-MGMTCredential -SystemType VMware_ESXi -SystemName VMware_ESXi
+    }|Where-Object{($_).credential}|Select-Object -First 1).credential
 )
-if ($null -ne $Credential)
-{
-    credstore $ComputerName -Credential $Credential
-}
-else {
-    credstore $ComputerName
-    $Credential=Get-Variable -Scope global -Name "cred_$computername" -ValueOnly
-}
 $SSHSession = New-SSHSession -ComputerName $ComputerName -Credential $Credential -Force 
-Invoke-SSHCommand -Command {
-    rm -r /etc/vmware/license.cfg
-    cp /etc/vmware/.#license.cfg /etc/vmware/license.cfg
+Invoke-SSHCommand -Command @'
+    rm -r /etc/vmware/license.cfg -f
+    cp "/etc/vmware/.#license.cfg" /etc/vmware/license.cfg
     /etc/init.d/vpxa restart
-} -SSHSession $SSHSession
+'@ -SSHSession $SSHSession
 
 Remove-SSHSession -SSHSession $SSHSession
